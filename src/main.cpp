bbc4aaa3e33dacb350588a858dcbeb8c323a06d6
Daniel Piron <dannypiron@gmail.com>
2022-11-26 00:39:22 -0500

    Pull some camera logic into a struct

--- src/main.cpp
+++ src/main.cpp
@@ -18,6 +18,37 @@
 #include <sstream>
 #include <vector>
 
+struct Camera
+{
+    struct Viewport
+    {
+        float width;
+        float height;
+
+        Viewport(float w, float h) : width(w), height(h) {}
+        Viewport(int w, int h) : width(static_cast<float>(w)), height(static_cast<float>(h))
+        {
+        }
+    };
+
+    glm::vec3 position{0};
+    glm::vec3 upVector{0, 1, 0};
+    Viewport viewport{640, 480};
+    float fieldOfVision = 60.0f;
+    float nearPlaneDistance = 0.1f;
+    float farPlaneDistance = 100.f;
+
+    glm::mat4 lookAtMatrix(const glm::vec3 &target) const
+    {
+        return glm::lookAt(position, target, upVector);
+    }
+
+    glm::mat4 projectionMatrix() const
+    {
+        return glm::perspectiveFov(glm::radians(fieldOfVision), viewport.width, viewport.height, 0.1f, 100.0f);
+    }
+};
+
 struct Vertex
 {
     glm::vec3 position;
@@ -170,8 +201,10 @@ public:
             return;
         }
 
+        // TODO: shader->bind(); or activateShader(shader);
         glUseProgram(shader->glId());
 
+        // TODO: Move vertex data into files
         cubeBuffer = std::make_shared<applesauce::VertexBuffer<Vertex>>(std::initializer_list<Vertex>{
             {{0.5, 1.0, -0.5}, {0.0, 1.0, 0.0}},
             {{-0.5, 1.0, 0.5}, {0.0, 1.0, 0.0}},
@@ -616,6 +649,8 @@ public:
             {applesauce::VertexAttribute::normal, 3, offsetof(Vertex, normal), sizeof(Vertex)},
         };
 
+        // TODO: The "Vertex Array" construct probably needs to be composed of
+        // vertex arrays somehow.
         cubeMesh->addVertexBuffer(*cubeBuffer, desc);
         planeMesh->addVertexBuffer(*planeBuffer, desc);
         tenkMesh->addVertexBuffer(*tenkBuffer, desc);
@@ -691,12 +726,11 @@ public:
         window.clear({0.1f, 0.1f, 0.1f, 1.0f});
 
         const auto [width, height] = window.framebufferSize();
+        camera.viewport = {width, height};
 
-        glm::vec4 cameraPos = glm::yawPitchRoll(theta, pitch, 0.0f) * glm::vec4(0, 0, -dist, 0);
-        glm::mat4 view = glm::lookAt(glm::vec3(cameraPos),
-                                     glm::vec3(0, 0, 0),
-                                     glm::vec3(0, 1, 0));
-        glm::mat4 projection = glm::perspectiveFov(glm::radians(60.f), static_cast<float>(width), static_cast<float>(height), 0.1f, 100.0f);
+        camera.position = glm::mat3(glm::yawPitchRoll(theta, pitch, 0.0f)) * glm::vec3{0, 0, -dist};
+        glm::mat4 view = camera.lookAtMatrix(glm::vec3(0));
+        glm::mat4 projection = camera.projectionMatrix();
 
         if (lastTank)
         {
@@ -755,6 +789,7 @@ private:
     double last_xpos = 0;
     double last_ypos = 0;
     bool move_camera = false;
+    Camera camera;
 };
 
 int main()
