28a49e746e7e07f4ef6c35bf172cb0e14700eba6
Daniel Piron <dannypiron@gmail.com>
2022-12-03 10:36:00 -0500

    Parse buffer view

--- tests/test_glTFLoading.cpp
+++ tests/test_glTFLoading.cpp
@@ -2,9 +2,12 @@
 
 #include <nlohmann/json.hpp>
 
-class glTF
+#include <string>
+#include <vector>
+
+struct glTF
 {
-	class Asset
+	struct Asset
 	{
 	public:
 		class Version
@@ -41,24 +44,95 @@ class glTF
 	};
 
 public:
-	glTF(const char *json_text)
+	struct BufferView
 	{
-		auto j = nlohmann::json::parse(json_text);
-		asset = Asset(j.at("asset").at("version").get<std::string>());
-	}
+		int buffer;
+		int byteOffset;
+		int byteLength;
+		int target;
+
+		bool operator==(const BufferView &rhs) const
+		{
+			return buffer == rhs.buffer && byteOffset == rhs.byteOffset &&
+				   byteLength == rhs.byteLength && target == rhs.target;
+		}
+	};
 
 public:
 	Asset asset;
+	std::vector<BufferView> bufferViews;
 };
 
-using namespace nlohmann;
+void from_json(const nlohmann::json &j, glTF::Asset::Version &v)
+{
+	v = glTF::Asset::Version(j.get<std::string>());
+}
+
+void from_json(const nlohmann::json &j, glTF::Asset &a)
+{
+	j.at("version").get_to(a.version);
+}
+
+void from_json(const nlohmann::json &j, glTF::BufferView &bv)
+{
+	j.at("buffer").get_to(bv.buffer);
+	j.at("byteOffset").get_to(bv.byteOffset);
+	j.at("byteLength").get_to(bv.byteLength);
+	j.at("target").get_to(bv.target);
+}
+
+void from_json(const nlohmann::json &j, glTF &gltf)
+{
+	j.at("asset").get_to(gltf.asset);
+	if (j.count("bufferViews"))
+	{
+		j.at("bufferViews").get_to(gltf.bufferViews);
+	}
+}
+
+glTF glTFFromString(const char *jsonText)
+{
+	glTF gltf;
+	auto j = nlohmann::json::parse(jsonText);
+	j.get_to(gltf);
+	return gltf;
+}
 
 TEST(glTFLoader, CanParseAssetVersion)
 {
 	const char *asset_version_only = R"({ "asset": { "version": "2.0" } })";
-	glTF gltf(asset_version_only);
+	auto gltf = glTFFromString(asset_version_only);
 
 	EXPECT_EQ(std::string("2.0"), static_cast<std::string>(gltf.asset.version));
 	EXPECT_EQ(2, gltf.asset.version.major());
 	EXPECT_EQ(0, gltf.asset.version.minor());
+}
+
+TEST(glTFLoader, CanParseBufferViews)
+{
+	const char *bufferViews = R"({
+			"asset": { "version": "2.0" }, 
+			"bufferViews": [
+			  {
+				"buffer": 0,
+				"byteOffset": 0,
+				"byteLength": 6,
+				"target": 34963
+              },
+			  {
+			    "buffer": 0,
+			    "byteOffset": 8,
+			    "byteLength": 36,
+			    "target": 34962
+			  }
+		    ]
+		  })";
+	auto gltf = glTFFromString(bufferViews);
+
+	glTF::BufferView firstView{0, 0, 6, 34963};
+	glTF::BufferView secondView{0, 8, 36, 34962};
+
+	ASSERT_EQ(2, gltf.bufferViews.size());
+	EXPECT_EQ(firstView, gltf.bufferViews[0]);
+	EXPECT_EQ(secondView, gltf.bufferViews[1]);
 }
