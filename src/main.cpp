2a066d9a1951da5de73b6c53824683c3b9245da5
Daniel Piron <dannypiron@gmail.com>
2022-12-16 22:46:01 -0500

    Make a box mesh

--- src/main.cpp
+++ src/main.cpp
@@ -126,6 +126,145 @@ Mesh makePlaneMesh(float planeSize)
     return meshFromComponents(vertices, normals, texcoords, indices);
 }
 
+Mesh makeBoxMesh(float boxSize)
+{
+    //
+    //
+    //         6----------7
+    //        /|         /|
+    //       / |        / |
+    //      3----------2  |
+    //      |  |       |  |
+    //      |  |       |  |
+    //      |  5-------|--4
+    //      | /        | /
+    //      |/         |/
+    //      0----------1
+    //
+    //
+
+    const float hSize = boxSize;
+    std::vector<glm::vec3> corner{
+        // Near corners
+        {-hSize, -hSize, hSize}, // 0
+        {hSize, -hSize, hSize},  // 1
+        {hSize, hSize, hSize},   // 2
+        {-hSize, hSize, hSize},  // 3
+        // Far corners
+        {hSize, -hSize, -hSize},  // 4
+        {-hSize, -hSize, -hSize}, // 5
+        {-hSize, hSize, -hSize},  // 6
+        {hSize, hSize, -hSize},   // 7
+    };
+
+    std::vector<glm::vec3> sideNormal{
+        {0, 0, 1.0f},  // near
+        {0, 0, -1.0f}, // far
+        {-1.0f, 0, 0}, // left
+        {1.0f, 0, 0},  // right
+        {0, 1.0f, 0},  // top
+        {0, -1.0f, 0}, // bottom
+    };
+
+    std::vector<glm::vec3> vertices{
+        corner[0], corner[1], corner[2], corner[3], // near    0-3
+        corner[4], corner[5], corner[6], corner[7], // far     4-7
+        corner[5], corner[0], corner[3], corner[6], // left    8-11
+        corner[1], corner[4], corner[7], corner[2], // right   12-15
+        corner[3], corner[2], corner[7], corner[6], // top     16-19
+        corner[1], corner[0], corner[5], corner[4], // bottom  20-23
+    };
+
+    std::vector<glm::vec3> normals{
+        sideNormal[0], sideNormal[0], sideNormal[0], sideNormal[0], // near
+        sideNormal[1], sideNormal[1], sideNormal[1], sideNormal[1], // far
+        sideNormal[2], sideNormal[2], sideNormal[2], sideNormal[2], // left
+        sideNormal[3], sideNormal[3], sideNormal[3], sideNormal[3], // right
+        sideNormal[4], sideNormal[4], sideNormal[4], sideNormal[4], // top
+        sideNormal[5], sideNormal[5], sideNormal[5], sideNormal[5], // bottom
+    };
+
+    std::vector<glm::vec2> texcoords{
+        // near
+        {0, 1},
+        {1, 1},
+        {1, 0},
+        {0, 0},
+        // far
+        {0, 1},
+        {1, 1},
+        {1, 0},
+        {0, 0},
+        // left
+        {0, 1},
+        {1, 1},
+        {1, 0},
+        {0, 0},
+        // right
+        {0, 1},
+        {1, 1},
+        {1, 0},
+        {0, 0},
+        // top
+        {0, 1},
+        {1, 1},
+        {1, 0},
+        {0, 0},
+        // bottom
+        {0, 1},
+        {1, 1},
+        {1, 0},
+        {0, 0},
+    };
+
+    std::vector<uint16_t> indices{
+        // near
+        0,
+        1,
+        2,
+        0,
+        2,
+        3,
+        // far
+        4,
+        5,
+        6,
+        4,
+        6,
+        7,
+        // left
+        8,
+        9,
+        10,
+        8,
+        10,
+        11,
+        // right
+        12,
+        13,
+        14,
+        12,
+        14,
+        15,
+        // top
+        16,
+        17,
+        18,
+        16,
+        18,
+        19,
+        // bottom
+        20,
+        21,
+        22,
+        20,
+        22,
+        23,
+    };
+
+    return meshFromComponents(vertices, normals, texcoords, indices);
+}
+
 static constexpr unsigned int SHADOW_WIDTH = 1024,
                               SHADOW_HEIGHT = 1024;
 
@@ -236,7 +375,7 @@ public:
 
         camera.fieldOfVision = 42.0f;
 
-        auto plane = std::make_shared<Mesh>(makePlaneMesh(20));
+        auto plane = std::make_shared<Mesh>(makeBoxMesh(1));
         entities.push_back({glm::vec3{0}, plane});
 
         // Init shadow mapping bits
