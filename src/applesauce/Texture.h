be9d027ad053f6b2b6909c748122e2827d182560
Daniel Piron <dannypiron@gmail.com>
2022-12-19 10:31:00 -0500

    Set texture filters

--- src/applesauce/Texture.h
+++ src/applesauce/Texture.h
@@ -6,6 +6,68 @@ namespace applesauce
 {
     class Texture2D : public GLResource
     {
+    public:
+        enum class Format
+        {
+            rgba
+        };
+
+        enum class Filter
+        {
+            nearest,
+            linear,
+            nearestMipMapNearest,
+            nearestMipMapLinear,
+            linearMipMapNearest,
+            linearMipMapLinear,
+        };
+
+    private:
+        static bool minFilterOnly(const Filter filter)
+        {
+            switch (filter)
+            {
+            case Filter::nearestMipMapNearest:
+            case Filter::nearestMipMapLinear:
+            case Filter::linearMipMapNearest:
+            case Filter::linearMipMapLinear:
+                return true;
+            default:
+                return false;
+            }
+        }
+
+        static GLenum glFilter(const Filter filter)
+        {
+            switch (filter)
+            {
+            case Filter::nearest:
+                return GL_NEAREST;
+            case Filter::linear:
+                return GL_LINEAR;
+            case Filter::nearestMipMapNearest:
+                return GL_NEAREST_MIPMAP_NEAREST;
+            case Filter::nearestMipMapLinear:
+                return GL_NEAREST_MIPMAP_LINEAR;
+            case Filter::linearMipMapNearest:
+                return GL_LINEAR_MIPMAP_NEAREST;
+            case Filter::linearMipMapLinear:
+                return GL_LINEAR_MIPMAP_LINEAR;
+            default:
+                return 0;
+            }
+        }
+
+        static GLenum glInternalFormat(const Format format)
+        {
+            switch (format)
+            {
+            case Format::rgba:
+                return GL_RGBA;
+            default:
+                return 0;
+            }
+        }
         static GLuint genGLTexture()
         {
             GLuint result;
@@ -16,11 +78,45 @@ namespace applesauce
         static constexpr GLenum target = GL_TEXTURE_2D;
 
     public:
-        Texture2D() : GLResource(genGLTexture()) {}
+        Texture2D(Format format = Format::rgba) : GLResource(genGLTexture()), internalFormat(format) {}
+
         ~Texture2D()
         {
             auto id = glId();
             glDeleteTextures(1, &id);
+        }
+
+        void setImage(int width, int height, Format, void *data) const
+        {
+            bind();
+            glTexImage2D(target,
+                         0, // mipmap level
+                         glInternalFormat(internalFormat),
+                         static_cast<GLsizei>(width),
+                         static_cast<GLsizei>(height),
+                         0, // border always 0
+                         GL_RGBA,
+                         GL_UNSIGNED_BYTE,
+                         data);
+            unbind();
+        }
+
+        void setMinFilter(const Filter filter)
+        {
+            bind();
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, glFilter(filter));
+            unbind();
+        }
+
+        void setMagFilter(const Filter filter)
+        {
+            if (minFilterOnly(filter))
+            {
+                return;
+            }
+            bind();
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, glFilter(filter));
+            unbind();
         }
 
         void bind() const
@@ -32,5 +128,8 @@ namespace applesauce
         {
             glBindTexture(target, 0);
         }
+
+    private:
+        Format internalFormat;
     };
 }
--- tests/test_Texture.cpp
+++ tests/test_Texture.cpp
@@ -3,15 +3,19 @@
 #include "AppleSauceTest.h"
 #include <applesauce/Texture.h>
 
+#include <vector>
+
 class AppleSauceTexture : public AppleSauceTest
 {
 };
+
+using namespace applesauce;
 
 TEST_F(AppleSauceTexture, CanCreateNewTexture)
 {
     GLint boundTex = 0;
     {
-        applesauce::Texture2D tex;
+        Texture2D tex;
 
         tex.bind();
         glGetIntegerv(GL_TEXTURE_BINDING_2D, &boundTex);
@@ -22,4 +26,86 @@ TEST_F(AppleSauceTexture, CanCreateNewTexture)
         EXPECT_TRUE(glIsTexture(boundTex));
     }
     EXPECT_FALSE(glIsTexture(boundTex));
+}
+
+TEST_F(AppleSauceTexture, CanSetFilters)
+{
+    Texture2D tex;
+    tex.setMinFilter(Texture2D::Filter::nearest);
+    tex.setMagFilter(Texture2D::Filter::nearest);
+
+    tex.bind();
+
+    GLint minFilter;
+    glGetTexParameteriv(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, &minFilter);
+    GLint magFilter;
+    glGetTexParameteriv(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, &magFilter);
+
+    EXPECT_EQ(GL_NEAREST, minFilter);
+    EXPECT_EQ(GL_NEAREST, magFilter);
+
+    tex.setMinFilter(Texture2D::Filter::linear);
+    tex.setMagFilter(Texture2D::Filter::linear);
+
+    tex.bind();
+    glGetTexParameteriv(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, &minFilter);
+    EXPECT_EQ(GL_LINEAR, minFilter);
+
+    glGetTexParameteriv(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, &magFilter);
+    EXPECT_EQ(GL_LINEAR, magFilter);
+
+    tex.setMinFilter(Texture2D::Filter::nearestMipMapNearest);
+    tex.bind();
+
+    glGetTexParameteriv(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, &minFilter);
+    EXPECT_EQ(GL_NEAREST_MIPMAP_NEAREST, minFilter);
+
+    tex.setMinFilter(Texture2D::Filter::nearestMipMapLinear);
+    tex.bind();
+
+    glGetTexParameteriv(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, &minFilter);
+    EXPECT_EQ(GL_NEAREST_MIPMAP_LINEAR, minFilter);
+
+    tex.setMinFilter(Texture2D::Filter::linearMipMapNearest);
+    tex.bind();
+
+    glGetTexParameteriv(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, &minFilter);
+    EXPECT_EQ(GL_LINEAR_MIPMAP_NEAREST, minFilter);
+
+    tex.setMinFilter(Texture2D::Filter::linearMipMapLinear);
+    tex.bind();
+
+    glGetTexParameteriv(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, &minFilter);
+    EXPECT_EQ(GL_LINEAR_MIPMAP_LINEAR, minFilter);
+
+    tex.setMagFilter(Texture2D::Filter::linearMipMapLinear);
+    tex.bind();
+
+    glGetTexParameteriv(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, &magFilter);
+    EXPECT_EQ(GL_LINEAR, magFilter);
+}
+
+TEST_F(AppleSauceTexture, CanCreateNewRGBTexture)
+{
+    std::vector<uint8_t> textureData{
+        0xFF, 0x00, 0x00, 0xFF, // red dot
+        0x00, 0xFF, 0x00, 0xFF, // green dot
+        0x0F, 0x00, 0xFF, 0xFF, // blue dot
+        0xFF, 0xFF, 0x00, 0xFF, // yellow dot
+    };
+
+    Texture2D tex(Texture2D::Format::rgba);
+    tex.setImage(2, 2, Texture2D::Format::rgba, &textureData[0]);
+
+    tex.bind();
+    GLint internalFormat;
+    glGetTexLevelParameteriv(GL_TEXTURE_2D, 0, GL_TEXTURE_INTERNAL_FORMAT, &internalFormat);
+
+    EXPECT_EQ(GL_RGBA8, internalFormat);
+
+    std::vector<uint8_t> buffer(textureData.size());
+
+    glGetTexImage(GL_TEXTURE_2D, 0, GL_RGBA, GL_UNSIGNED_BYTE, &buffer[0]);
+
+    EXPECT_EQ(textureData, buffer);
 }
