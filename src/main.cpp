42427a6a050dad8a6440cfbef6414400909e5c8b
Daniel Piron <dannypiron@gmail.com>
2022-12-29 21:55:26 -0500

    Move "renderQuad" to Debug.h

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #define _USE_MATH_DEFINES
 
 #include "applesauce/App.h"
+#include "applesauce/Debug.h"
 #include "applesauce/Entity.h"
 #include "applesauce/Input.h"
 #include "applesauce/VertexBuffer.h"
@@ -36,49 +37,6 @@
 #include <unordered_map>
 #include <vector>
 
-static unsigned int quadVAO;
-static unsigned int quadVBO;
-void renderQuad()
-{
-    if (quadVAO == 0)
-    {
-        float quadVertices[] = {
-            -0.5f,
-            0.5f,
-            0.0f,
-            0.0f,
-            1.0f,
-            -0.5f,
-            -0.5f,
-            0.0f,
-            0.0f,
-            0.0f,
-            0.5f,
-            0.5f,
-            0.0f,
-            1.0f,
-            1.0f,
-            0.5f,
-            -0.5f,
-            0.0f,
-            1.0f,
-            0.0f,
-        };
-        // setup plane VAO
-        glGenVertexArrays(1, &quadVAO);
-        glGenBuffers(1, &quadVBO);
-        glBindVertexArray(quadVAO);
-        glBindBuffer(GL_ARRAY_BUFFER, quadVBO);
-        glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), &quadVertices, GL_STATIC_DRAW);
-        glEnableVertexAttribArray(0);
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void *)0);
-        glEnableVertexAttribArray(1);
-        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void *)(3 * sizeof(float)));
-    }
-    glBindVertexArray(quadVAO);
-    glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
-    glBindVertexArray(0);
-}
 
 static constexpr unsigned int SHADOW_WIDTH = 2048,
                               SHADOW_HEIGHT = 2048;
