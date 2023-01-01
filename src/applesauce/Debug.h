75e462c7aa106e0de690f48f620697e983bababc
Daniel Piron <dannypiron@gmail.com>
2023-01-01 13:42:31 -0500

    Place renderQuad into a struct and correct build issues

--- src/applesauce/Debug.h
+++ src/applesauce/Debug.h
@@ -2,46 +2,35 @@
 
 #include <glad/glad.h>
 
-static unsigned int quadVAO;
-static unsigned int quadVBO;
-static void renderQuad()
-{
-    if (quadVAO == 0)
-    {
-        float quadVertices[] = {
-            -0.5f,
-            0.5f,
-            0.0f,
-            0.0f,
-            1.0f,
-            -0.5f,
-            -0.5f,
-            0.0f,
-            0.0f,
-            0.0f,
-            0.5f,
-            0.5f,
-            0.0f,
-            1.0f,
-            1.0f,
-            0.5f,
-            -0.5f,
-            0.0f,
-            1.0f,
-            0.0f,
-        };
-        // setup plane VAO
-        glGenVertexArrays(1, &quadVAO);
-        glGenBuffers(1, &quadVBO);
-        glBindVertexArray(quadVAO);
-        glBindBuffer(GL_ARRAY_BUFFER, quadVBO);
-        glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), &quadVertices, GL_STATIC_DRAW);
-        glEnableVertexAttribArray(0);
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
-        glEnableVertexAttribArray(1);
-        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+namespace applesauce {
+
+struct Debug {
+
+  static unsigned int quadVAO;
+  static unsigned int quadVBO;
+  static void renderQuad() {
+    if (quadVAO == 0) {
+      float quadVertices[] = {
+          -0.5f, 0.5f, 0.0f, 0.0f, 1.0f, -0.5f, -0.5f, 0.0f, 0.0f, 0.0f,
+          0.5f,  0.5f, 0.0f, 1.0f, 1.0f, 0.5f,  -0.5f, 0.0f, 1.0f, 0.0f,
+      };
+      // setup plane VAO
+      glGenVertexArrays(1, &quadVAO);
+      glGenBuffers(1, &quadVBO);
+      glBindVertexArray(quadVAO);
+      glBindBuffer(GL_ARRAY_BUFFER, quadVBO);
+      glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), &quadVertices,
+                   GL_STATIC_DRAW);
+      glEnableVertexAttribArray(0);
+      glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float),
+                            (void *)0);
+      glEnableVertexAttribArray(1);
+      glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float),
+                            (void *)(3 * sizeof(float)));
     }
     glBindVertexArray(quadVAO);
     glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
     glBindVertexArray(0);
-}
+  }
+};
+} // namespace applesauce
